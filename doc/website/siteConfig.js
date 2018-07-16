4e429999a0ef698a6fb3947a640fe7228ee0a052
Anton Bachin <antonbachin@yahoo.com>
2018-07-16 17:22:48 -0500

    Docs: add search
    [skip ci]

--- doc/website/siteConfig.js
+++ doc/website/siteConfig.js
@@ -12,6 +12,7 @@ const siteConfig = {
   headerLinks: [
     {doc: 'QuickStart', label: 'Docs'},
     {doc: 'API', label: 'API'},
+    {search: true},
     {href: "https://github.com/aantron/repromise", label: "GitHub"},
   ],
 
@@ -31,6 +32,11 @@ const siteConfig = {
   //   repoUrl: 'https://github.com/facebook/test-site',
 
   cleanUrl: true,
+
+  algolia: {
+    apiKey: "e0a7347e16ff47590833503d435facf2",
+    indexName: "repromise",
+  },
 };
 
 module.exports = resaurus(siteConfig);
