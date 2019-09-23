37b4a64ab681125193ecc5fd1c08cd4d862bb2bf
Anton Bachin <antonbachin@yahoo.com>
2019-09-23 09:06:16 -0500

    Compress bundles in the bundle size test

--- test/bundle/size.sh
+++ test/bundle/size.sh
@@ -11,16 +11,18 @@ npx webpack \
   --display none --mode production --optimize-minimize \
   --entry ./lib/js/test/bundle/uses_promise.js \
   --output ./test/bundle/uses_promise.js
+gzip -9f test/bundle/control.js
+gzip -9f test/bundle/uses_promise.js
 
-CONTROL=`stat -c '%s' test/bundle/control.js`
-PROMISE=`stat -c '%s' test/bundle/uses_promise.js`
+CONTROL=`stat -c '%s' test/bundle/control.js.gz`
+PROMISE=`stat -c '%s' test/bundle/uses_promise.js.gz`
 DIFFERENCE=`expr $PROMISE - $CONTROL`
-LIMIT=4096
+LIMIT=1024
 
 if [ $DIFFERENCE -gt $LIMIT ]
 then
-  echo "Bundle size $DIFFERENCE exceeds $LIMIT"
+  echo "Bundle size (incremental) $DIFFERENCE exceeds $LIMIT"
   exit 1
 else
-  echo "Bundle size: $DIFFERENCE"
+  echo "Bundle size (incremental): $DIFFERENCE"
 fi
